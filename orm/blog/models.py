d7136b57d4e839e596ec064514c8a6c7944984f8
keinuma <nununu.mono@gmail.com>
2018-12-15 18:20:58 +0900

    Add dummy data and model setting

--- orm/blog/models.py
+++ orm/blog/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 
-class Blog(models.Model):
-    name = models.CharField(max_length=255)
-    url = models.URLField()
-
-
 class Post(models.Model):
-    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     text = models.TextField()
-    opened_at = models.DateTimeField()
 
 
 class Comment(models.Model):
--- orm/config/settings.py
+++ orm/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FIXTURE_DIRS = 'blog/fixtures/'
